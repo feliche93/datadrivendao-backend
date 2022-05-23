4c9c30fc21e1fd4b4845c18ff8ef794abe8d000b
Felix Vemmer <felix.vemmer@gmail.com>
2022-05-23 13:55:24 +0000

    feat: refactors twitter history

--- dbt/models/marts/fct_twitter_history.sql
+++ dbt/models/marts/fct_twitter_history.sql
@@ -1,40 +1,27 @@
-{{ config(materialized='table') }}
 
-WITH dim_dao AS (
-    SELECT *
-    FROM {{ ref('dim_dao') }}
-),
-
-twitter_users_scd AS (
+WITH twitter_users AS (
     SELECT * 
-    FROM {{ source('raw_data', 'twitter_users_scd') }}
+    FROM {{ source('twitter', 'users') }}
 ),
 
 transformed AS (
-
     SELECT DISTINCT
-        dao_id,
-        CAST(
-            PARSE_TIMESTAMP(
-            "%F",
-            SUBSTR(twitter_users_scd.scraped_at, 0, 10)
-        ) AS DATE) AS scraped_date,
-        twitter_users_scd.followers_count AS total_followers,
-        twitter_users_scd.friends_count AS total_friends,
-        twitter_users_scd.listed_count AS total_listed,
-        twitter_users_scd.favourites_count AS total_favourites,
-        twitter_users_scd.statuses_count AS total_statuses
-    FROM twitter_users_scd
-    INNER JOIN dim_dao ON twitter_users_scd.screen_name = dim_dao.twitter
-    WHERE scraped_at IS NOT NULL
-    
+        screen_name,
+        CAST(scraped_at AS DATE) AS scraped_date,
+        followers_count AS total_followers,
+        friends_count AS total_friends,
+        listed_count AS total_listed,
+        favourites_count AS total_favourites,
+        statuses_count AS total_statuses
+    FROM twitter_users
 )
 
 SELECT 
     transformed.*,
-    total_followers - LEAD(total_followers, 1) OVER (PARTITION BY dao_id ORDER BY scraped_date DESC) AS followers,
-    total_friends - LEAD(total_friends, 1) OVER (PARTITION BY dao_id ORDER BY scraped_date DESC) AS friends,
-    total_listed - LEAD(total_listed, 1) OVER (PARTITION BY dao_id ORDER BY scraped_date DESC) AS listed,
-    total_favourites - LEAD(total_favourites, 1) OVER (PARTITION BY dao_id ORDER BY scraped_date DESC) AS favourites,
-    total_statuses - LEAD(total_statuses, 1) OVER (PARTITION BY dao_id ORDER BY scraped_date DESC) AS statuses,
+    total_followers - LEAD(total_followers, 1) OVER (PARTITION BY screen_name ORDER BY scraped_date DESC) AS followers,
+    total_friends - LEAD(total_friends, 1) OVER (PARTITION BY screen_name ORDER BY scraped_date DESC) AS friends,
+    total_listed - LEAD(total_listed, 1) OVER (PARTITION BY screen_name ORDER BY scraped_date DESC) AS listed,
+    total_favourites - LEAD(total_favourites, 1) OVER (PARTITION BY screen_name ORDER BY scraped_date DESC) AS favourites,
+    total_statuses - LEAD(total_statuses, 1) OVER (PARTITION BY screen_name ORDER BY scraped_date DESC) AS statuses,
 FROM transformed
+WHERE screen_name = 'developer_dao'
